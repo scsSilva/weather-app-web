86cbf1dcbaacb748e3e85d9ccbbe00f6aa0e10fd
Sallys Carlos <sallys.carlos93@gmail.com>
2023-02-23 15:57:20 -0300

    feat: fetch data api

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState, useRef } from "react";
 import Icon from "./assets/images/icon_weather.svg";
 import Illustration from "./assets/images/illustration_weather.svg";
 import * as Styles from "./styles";
@@ -12,65 +13,125 @@ import {
   Tooltip,
   ResponsiveContainer,
 } from "recharts";
+import axios from "axios";
+import { Weather } from "./types/weather";
 
 function App() {
-  const data = [
-    { day: "23/02", temp: 30 },
-    { day: "24/02", temp: 25 },
-    { day: "25/02", temp: 27 },
-  ];
+  const [data, setData] = useState<Weather>();
+  const [loading, setLoading] = useState(false);
+  const input = useRef<HTMLInputElement>(null);
+
+  const getWeatherInformationFromCurrentLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(async (position) => {
+        const request = await axios.get(
+          `https://api.weatherapi.com/v1/forecast.json?key=${
+            import.meta.env.VITE_API_KEY
+          }&q=${position.coords.latitude},${
+            position.coords.longitude
+          }&days=7&lang=pt`
+        );
+
+        setData(request.data);
+      });
+    }
+  };
+
+  const getWeatherInformationFromInputLocation = async () => {
+    if (input.current?.value != "") {
+      setLoading(true);
+
+      const request = await axios.get(
+        `https://api.weatherapi.com/v1/forecast.json?key=${
+          import.meta.env.VITE_API_KEY
+        }&q=${input.current!.value}&days=7&lang=pt`
+      );
+
+      setData(request.data);
+      setLoading(false);
+      input.current!.value = "";
+    }
+  };
 
   return (
     <Styles.Container>
       <Styles.Content>
         <Styles.InfoSection>
-          {/* <Styles.IllustrationImage
-            src={Illustration}
-            alt="Ilustração de um sistema de previsão do tempo"
-          /> */}
-          <Styles.HeaderInfoSection>
-            <h1>Maceió</h1>
-            <p>23/01/32</p>
-          </Styles.HeaderInfoSection>
-          <Styles.OverviewSection>
-            <Styles.OverviewItem>
-              <div>
-                <CiTempHigh size={30} />
-                <Styles.TitleOverviewItem>Temperatura</Styles.TitleOverviewItem>
-              </div>
-              <Styles.ValueOverviewItem>30°</Styles.ValueOverviewItem>
-            </Styles.OverviewItem>
-            <Styles.OverviewItem>
-              <div>
-                <WiHumidity size={30} />
-                <Styles.TitleOverviewItem>Umidade</Styles.TitleOverviewItem>
-              </div>
-              <Styles.ValueOverviewItem>66%</Styles.ValueOverviewItem>
-            </Styles.OverviewItem>
-            <Styles.OverviewItem>
-              <div>
-                <WiStrongWind size={30} />
-                <Styles.TitleOverviewItem>Vento</Styles.TitleOverviewItem>
-              </div>
-              <Styles.ValueOverviewItem>10.2 Km/h</Styles.ValueOverviewItem>
-            </Styles.OverviewItem>
-            <Styles.OverviewItem>
-              <div>
-                <WiDaySunny size={30} />
-                <Styles.TitleOverviewItem>UV</Styles.TitleOverviewItem>
-              </div>
-              <Styles.ValueOverviewItem>7.0</Styles.ValueOverviewItem>
-            </Styles.OverviewItem>
-          </Styles.OverviewSection>
-          <ResponsiveContainer width="100%" height="50%">
-            <LineChart data={data} margin={{ left: 0, top: 20 }}>
-              <Line type="monotone" dataKey="temp" stroke="#6c5ce7" />
-              <CartesianGrid stroke="#fafafa" strokeDasharray="5 5" />
-              <XAxis dataKey="day" />
-              <YAxis />
-              <Tooltip />
-            </LineChart>
-          </ResponsiveContainer>
+          {data == null ? (
+            <Styles.IllustrationImage
+              src={Illustration}
+              alt="Ilustração de um sistema de previsão do tempo"
+            />
+          ) : (
+            <>
+              <Styles.HeaderInfoSection>
+                <h1>{data?.location.name}</h1>
+                <p>
+                  {data?.location.localtime
+                    .split(" ")[0]
+                    .split("-")
+                    .reverse()
+                    .join("/")}
+                </p>
+              </Styles.HeaderInfoSection>
+              <Styles.OverviewSection>
+                <Styles.OverviewItem>
+                  <div>
+                    <CiTempHigh size={30} />
+                    <Styles.TitleOverviewItem>
+                      Temperatura
+                    </Styles.TitleOverviewItem>
+                  </div>
+                  <Styles.ValueOverviewItem>
+                    {data.current.temp_c}°
+                  </Styles.ValueOverviewItem>
+                </Styles.OverviewItem>
+                <Styles.OverviewItem>
+                  <div>
+                    <WiHumidity size={30} />
+                    <Styles.TitleOverviewItem>Umidade</Styles.TitleOverviewItem>
+                  </div>
+                  <Styles.ValueOverviewItem>
+                    {data.current.humidity}%
+                  </Styles.ValueOverviewItem>
+                </Styles.OverviewItem>
+                <Styles.OverviewItem>
+                  <div>
+                    <WiStrongWind size={30} />
+                    <Styles.TitleOverviewItem>Vento</Styles.TitleOverviewItem>
+                  </div>
+                  <Styles.ValueOverviewItem>
+                    {data.current.wind_kph} Km/h
+                  </Styles.ValueOverviewItem>
+                </Styles.OverviewItem>
+                <Styles.OverviewItem>
+                  <div>
+                    <WiDaySunny size={30} />
+                    <Styles.TitleOverviewItem>UV</Styles.TitleOverviewItem>
+                  </div>
+                  <Styles.ValueOverviewItem>
+                    {data.current.uv}
+                  </Styles.ValueOverviewItem>
+                </Styles.OverviewItem>
+              </Styles.OverviewSection>
+              {/* <ResponsiveContainer width="100%" height="50%">
+                <LineChart
+                  data={data?.forecast.forecastday}
+                  margin={{ left: 0, top: 20 }}
+                >
+                  <Line
+                    type="monotone"
+                    dataKey={data?.forecast.forecastday.day.maxtemp_c}
+                    stroke="#6c5ce7"
+                  />
+                  <CartesianGrid stroke="#fafafa" strokeDasharray="5 5" />
+                  <XAxis dataKey="day" />
+                  <YAxis />
+                  <Tooltip />
+                </LineChart>
+              </ResponsiveContainer> */}
+            </>
+          )}
         </Styles.InfoSection>
 
         <Styles.SearchSection>
@@ -78,16 +139,23 @@ function App() {
             src={Icon}
             alt="Ícone representando previsão do tempo"
           />
-          <Styles.BtnLocalization>
+          <Styles.BtnLocalization
+            onClick={getWeatherInformationFromCurrentLocation}
+          >
             Usar localização atual
           </Styles.BtnLocalization>
           <Styles.Form>
             <Styles.InputSearch
-              id="location_field"
+              ref={input}
               placeholder="Informe o local desejado"
               type="text"
             />
-            <Styles.BtnSearch>Pesquisar</Styles.BtnSearch>
+            <Styles.BtnSearch
+              disabled={loading}
+              onClick={getWeatherInformationFromInputLocation}
+            >
+              Pesquisar
+            </Styles.BtnSearch>
           </Styles.Form>
         </Styles.SearchSection>
       </Styles.Content>
